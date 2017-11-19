4fc2a865ff094371d26f7390fbe6254445f32772
tysky <dtysky51@gmail.com>
2017-11-19 21:34:19 +0300

    add tests for posts

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,50 @@
 import request from 'supertest';
+import matchers from 'jest-supertest-matchers';
+
 import app from '../src';
 
-test('request', async () => {
-  const res = await request(app).get('/');
-  if (res.error) {
-    throw new Error('Error detected!');
-  }
-  expect(res.status).toBe(200);
+describe('requests', () => {
+  beforeAll(() => {
+    jasmine.addMatchers(matchers); //eslint-disable-line
+  });
+
+  test('GET /', async () => {
+    const res = await request(app).get('/');
+    expect(res).toHaveHTTPStatus(200);
+  });
+
+  it('GET /posts', async () => {
+    const res = await request(app).get('/posts');
+    expect(res).toHaveHTTPStatus(200);
+  });
+
+  it('GET /posts/new', async () => {
+    const res = await request(app)
+      .get('/posts/new');
+    expect(res).toHaveHTTPStatus(200);
+  });
+
+  it('POST /posts', async () => {
+    const res = await request(app)
+      .post('/posts')
+      .type('form')
+      .send({ title: 'post title', body: 'post body' });
+    expect(res).toHaveHTTPStatus(302);
+  });
+
+  it('POST /posts (errors)', async () => {
+    const res = await request(app)
+      .post('/posts');
+    expect(res).toHaveHTTPStatus(422);
+  });
+
+  it('GET /posts/:id', async () => {
+    const query = request(app);
+    const res1 = await query
+      .post('/posts')
+      .type('form')
+      .send({ title: 'post title', body: 'post body' });
+    const res2 = await query.get(res1.headers.location);
+    expect(res2).toHaveHTTPStatus(200);
+  });
 });

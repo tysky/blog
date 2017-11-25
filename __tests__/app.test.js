6f9fbefc829b112fadf48da5690824c41c565cf0
tysky <dtysky51@gmail.com>
2017-11-26 00:13:31 +0300

    add tests for editing and deleting posts

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,4 +47,52 @@ describe('requests', () => {
     const res2 = await query.get(res1.headers.location);
     expect(res2).toHaveHTTPStatus(200);
   });
+
+  it('GET posts/:id/edit', async () => {
+    // const app = solution();
+    const res1 = await request(app)
+      .post('/posts')
+      .type('form')
+      .send({ title: 'post title', body: 'post body' });
+    expect(res1).toHaveHTTPStatus(302);
+    const url = res1.headers.location;
+    const res2 = await request(app)
+      .get(url);
+    expect(res2).toHaveHTTPStatus(200);
+  });
+
+  it('PATCH posts/:id', async () => {
+    const res1 = await request(app)
+      .post('/posts')
+      .type('form')
+      .send({ title: 'post title', body: 'post body' });
+    const url = res1.headers.location.match(/\/posts\/\d+/)[0];
+    const res2 = await request(app)
+      .patch(url)
+      .type('form')
+      .send({ title: 'new post title', body: 'new post body' });
+    expect(res2).toHaveHTTPStatus(302);
+  });
+
+  it('PATCH posts/:id (unproccessable entity)', async () => {
+    const res1 = await request(app)
+      .post('/posts')
+      .type('form')
+      .send({ title: 'post title', body: 'post body' });
+    const url = res1.headers.location.match(/\/posts\/\d+/)[0];
+    const res2 = await request(app)
+      .patch(url);
+    expect(res2).toHaveHTTPStatus(422);
+  });
+
+  it('DELETE posts/:id', async () => {
+    const res1 = await request(app)
+      .post('/posts')
+      .type('form')
+      .send({ title: 'post title', body: 'post body' });
+    const url = res1.headers.location.match(/\/posts\/\d+/)[0];
+    const res2 = await request(app)
+      .delete(url);
+    expect(res2).toHaveHTTPStatus(302);
+  });
 });

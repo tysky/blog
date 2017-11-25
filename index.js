4ffd44388a59c842b0369a9a2ebab710c3e750e2
tysky <dtysky51@gmail.com>
2017-11-26 00:14:31 +0300

    add editing and deleting posts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import methodOverride from 'method-override';
 
 import Post from './entities/Post';
 
@@ -9,9 +10,11 @@ const app = new Express();
 const logger = morgan('combined');
 app.use(logger);
 app.set('view engine', 'pug');
+// app.use('/assets', Express.static(process.env.NODE_PATH));
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-let posts = [
+let posts = [ //eslint-disable-line
   new Post('hello', 'how are your?'),
   new Post('nodejs', 'story about nodejs'),
 ];
@@ -47,6 +50,35 @@ app.post('/posts', (req, res) => {
     posts.push(newPost);
     res.redirect(302, `/posts/${newPost.id}`);
   }
+});
+
+app.get('/posts/:id/edit', (req, res) => {
+  const post = posts.find(_post => _post.id.toString() === req.params.id);
+  const { id, title, body } = post;
+  res.render('posts/edit', { post, form: { id, title, body }, errors: {} });
+});
+
+app.patch('/posts/:id', (req, res) => {
+  const { title, body } = req.body;
+  if (!title || !body) {
+    const error = {
+      title: 'Error! Title is empty!',
+      body: 'Error! Post is empty!',
+    };
+    res.status(422);
+    res.render('posts/show', error);
+  } else {
+    const post = posts.find(_post => _post.id.toString() === req.params.id);
+    post.title = title;
+    post.body = body;
+    res.redirect(302, `/posts/${post.id}`);
+  }
+});
+
+app.delete('/posts/:id', (req, res) => {
+  const post = posts.find(_post => _post.id.toString() === req.params.id);
+  posts = posts.filter(el => el !== post);
+  res.redirect(302, '/');
 });
 
 export default app;
